a7be7920ea4d8fc4a0486c3c9b385258207c9151
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2022-09-11 10:44:44 +0200

    CI: Add linting
    Snatched from 'https://www.alexedwards.net/blog/ci-with-go-and-github-actions'.

--- testutil/ci.go
+++ testutil/ci.go
@@ -2,9 +2,13 @@ package testutil
 
 import "os"
 
+// CI tries to detect if the test is being run on a CI server.
+// If so, a string identifying the CI is returned. The current implementation reports as follows:
+// - GitHub Actions: "github"
+// - No CI: ""
 func CI() string {
-    if _, ok := os.LookupEnv("GITHUB_WORKFLOW"); ok {
-        return "github"
-    }
-    return ""
+	if _, ok := os.LookupEnv("GITHUB_WORKFLOW"); ok {
+		return "github"
+	}
+	return ""
 }
