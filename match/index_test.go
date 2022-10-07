4b24c33ba004361a074c64b0d7fe10baa9edc4f8
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2022-10-07 16:58:28 +0200

    Refactor test constants

--- match/index_test.go
+++ match/index_test.go
@@ -9,19 +9,6 @@ import (
 	"github.com/bisgardo/dupe-nukem/scan"
 )
 
-//goland:noinspection GoSnakeCaseUsage
-var (
-	testdata_x_a = scan.NewFile("a", 2, 620331299357648818) // contents: "a"
-	testdata_x_b = scan.NewFile("b", 2, 623218616892763229) // contents: "b"
-	testdata_x_c = scan.NewFile("c", 2, 622257643729896040) // contents: "c"
-	testdata_y_a = scan.NewFile("a", 2, 620331299357648818) // contents: "a"
-	testdata_y_b = scan.NewFile("b", 2, 620331299357648818) // contents: "a"
-	testdata_y_c = scan.NewFile("c", 2, 617474768148124315) // contents: "d"
-
-	testdata_x = &scan.Dir{Name: "x", Files: []*scan.File{testdata_x_a, testdata_x_b, testdata_x_c}}
-	testdata_y = &scan.Dir{Name: "y", Files: []*scan.File{testdata_y_a, testdata_y_b, testdata_y_c}}
-)
-
 func Test__testdata_index(t *testing.T) {
 	root := "testdata"
 
---
+++ scan/testdata_test.go
@@ -0,0 +1,9 @@
+package scan
+
+//goland:noinspection GoSnakeCaseUsage
+var (
+	testdata_b_d   = &File{Name: "d", Size: 2, Hash: 644258871406045975} // contents: "x"
+	testdata_e_f_a = &File{Name: "a", Size: 2, Hash: 646158827499216133} // contents: "z"
+	testdata_a     = &File{Name: "a", Size: 2, Hash: 644258871406045975} // contents: "x"
+	testdata_c     = &File{Name: "c", Size: 2, Hash: 643306694336204474} // contents: "y"
+)
